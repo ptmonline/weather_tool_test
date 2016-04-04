72c166d9a3a180132bb997f0cc2ad7be4b91aa86
ptm <ptorresonline@gmail.com>
2016-04-04 16:58:22 +0200

    added user input

--- emailer.py
+++ emailer.py
@@ -1,4 +1,6 @@
 import requests
+import smtplib
+
 def get_emails():
     emails = {}
     try:
@@ -23,19 +25,39 @@ def get_schedule():
 def get_weather_forecast():
     api_key_config = open('config.txt', 'r')
     API_KEY = api_key_config.read()
-    API_KEY = API_KEY.strip()
-    api_key_load = {'appid' : API_KEY}
+    api_key_load = {'appid' : API_KEY.strip()}
     url = 'http://api.openweathermap.org/data/2.5/weather?q=Barcelona,spain'
     weather_request = requests.get(url, params=api_key_load)
     weather_json = weather_request.json()
+    description = weather_json['weather'][0]['description']
+    temp_max = weather_json['main']['temp_max']
+    temp_min = weather_json['main']['temp_min']
 
-    print(weather_json)
+    forecast = 'The Circus forecast for today is '
+    forecast += description + ' with a high of ' + str(int(temp_max))
+    forecast += ' and a low of '+ str(int(temp_min))
+
+    return forecast
+
+def send_emails(emails, schedule, forecast):
+    server = smtplib.SMTP('smtp.gmail.com', '587')
+    server.starttls()
+    from_email = input("What is your email? : ')
+    password = input("password?:")
+    server.login(from_email, password)
+    message = 'Subject: Welcome to the circus\n'
+    message += forecast
+    print(message)
+    server.sendmail(from_email, from_email, forecast)
+    server.quit()
     
 def main():
     emails = get_emails()
     print(emails)
     schedule = get_schedule()
     print(schedule)
-    get_weather_forecast()
+    forecast = get_weather_forecast()
+    print(forecast)
+    send_emails(emails, schedule, forecast)
     
 main()
